181ee97b2a146907b439602e88bd5b66de8d83fb
Ben Neill <benjaminrneill@gmail.com>
2023-09-10 11:28:21 +0100

    Update state.tf

--- state.tf
+++ state.tf
@@ -2,13 +2,16 @@ terraform {
   backend "azurerm" {}
 
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
     azuread = {
       source  = "hashicorp/azuread"
       version = "1.6.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.25"
     }
   }
 }
