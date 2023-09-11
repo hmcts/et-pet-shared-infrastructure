df910a593256d074a726e8a81af7cbbaccfe286b
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-09-11 15:39:28 +0100

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -11,11 +11,30 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
+provider "azurerm" {
+  alias           = "mgmt"
+  subscription_id = var.mgmt_subscription_id
+  features {}
+}
+
 data "azurerm_subnet" "private_endpoints" {
   provider             = azurerm.private_endpoints
   resource_group_name  = local.private_endpoint_rg_name
   virtual_network_name = local.private_endpoint_vnet_name
   name                 = "private-endpoints"
+}
+
+data "azurerm_virtual_network" "mgmt_vnet" {
+  provider            = azurerm.mgmt
+  name                = "cft-ptl-vnet"
+  resource_group_name = "cft-ptl-network-rg"
+}
+
+data "azurerm_subnet" "jenkins_subnet" {
+  provider             = azurerm.mgmt
+  name                 = "iaas"
+  virtual_network_name = data.azurerm_virtual_network.mgmt_vnet.name
+  resource_group_name  = data.azurerm_virtual_network.mgmt_vnet.resource_group_name
 }
 
 module "storage-account" {
