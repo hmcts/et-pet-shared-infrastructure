a8a02c0ffc2bdbaac50f3d97707f4c05f8cd4df2
Jason Paige <j@jasonpaige.co.uk>
2023-09-11 11:44:06 +0100

    Storage account with 2 containers

--- redis.tf
+++ redis.tf
@@ -1,14 +1,14 @@
 module "etapi-redis" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product  = var.product
-  name     = "${var.product}-api-${var.env}"
-  location = var.location
-  env      = var.env
-  private_endpoint_enabled = true
-  redis_version = "6"
-  business_area = "cft"
+  source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product                       = var.product
+  name                          = "${var.product}-api-${var.env}"
+  location                      = var.location
+  env                           = var.env
+  private_endpoint_enabled      = true
+  redis_version                 = "6"
+  business_area                 = "cft"
   public_network_access_enabled = false
-  common_tags  = var.common_tags
+  common_tags                   = var.common_tags
 }
 
 resource "azurerm_key_vault_secret" "redis6_access_key" {
--- variables.tf
+++ variables.tf
@@ -13,11 +13,11 @@ variable "location" {
   default = "UK South"
 }
 
-variable team_contact {
+variable "team_contact" {
   default = "#pet-devops"
 }
 
-variable team_name {
+variable "team_name" {
   default = "et-pet"
 }
 
@@ -41,6 +41,39 @@ variable "db_postgresql_user" {
 
 variable "db_storage_mb" {
   default = "5120"
+}
+
+variable "sa_account_kind" {
+  type        = string
+  default     = "BlobStorage"
+  description = "Defines the Kind of account. Valid options are Storage, StorageV2 and BlobStorage. Changing this forces a new resource to be created. Defaults to BlobStorage."
+
+  validation {
+    condition     = can(regex("^(Storage|StorageV2|BlobStorage)$", var.sa_account_kind))
+    error_message = "Invalid input, options: \"Storage\", \"StorageV2\", \"BlobStorage\"."
+  }
+}
+
+variable "sa_account_tier" {
+  type        = string
+  default     = "Standard"
+  description = "Defines the Tier to use for this storage account. Valid options are Standard and Premium. Changing this forces a new resource to be created. Defaults to Standard."
+
+  validation {
+    condition     = can(regex("^(Standard|Premium)$", var.sa_account_tier))
+    error_message = "Invalid input, options: \"Standard\", \"Premium\"."
+  }
+}
+
+variable "sa_account_replication_type" {
+  type        = string
+  default     = "GRS"
+  description = "Defines the type of replication to use for this storage account. Valid options are LRS, GRS, RAGRS and ZRS. Defaults to RAGRS."
+
+  validation {
+    condition     = can(regex("^(LRS|GRS|RAGRS|ZRS)$", var.sa_account_replication_type))
+    error_message = "Invalid input, options: \"LRS\", \"GRS\", \"RAGRS\", \"ZRS\"."
+  }
 }
 
 variable "subscription" {}
