af12ae1d67fe116ed8a94a6fff667ef168910c0e
Jason Paige <j@jasonpaige.co.uk>
2023-09-11 13:58:49 +0100

    TMP Remove SA

--- storage.tf
+++ storage.tf
@@ -17,26 +17,3 @@ data "azurerm_subnet" "private_endpoints" {
   virtual_network_name = local.private_endpoint_vnet_name
   name                 = "private-endpoints"
 }
-
-module "storage-account" {
-  source                     = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                        = var.env
-  storage_account_name       = replace("${var.product}sa${var.env}", "-", "")
-  resource_group_name        = azurerm_resource_group.rg.name
-  location                   = var.location
-  account_kind               = var.sa_account_kind
-  account_tier               = var.sa_account_tier
-  account_replication_type   = var.sa_account_replication_type
-  private_endpoint_subnet_id = data.azurerm_subnet.private_endpoints.id
-  common_tags                = var.common_tags
-  containers = [
-    {
-      name        = "public"
-      access_type = "private"
-    },
-    {
-      name        = "private"
-      access_type = "private"
-    }
-  ]
-}
