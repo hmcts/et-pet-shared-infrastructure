93be30b89942b063415cc097cf92df9d78656996
Jason Paige <j@jasonpaige.co.uk>
2023-09-13 11:10:06 +0100

    pg provider

--- data.tf
+++ data.tf
@@ -26,6 +26,13 @@ provider "azurerm" {
 provider "azurerm" {
   features {}
   skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
   alias                      = "aks"
   subscription_id            = var.aks_subscription_id
 }
